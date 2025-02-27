5db13f0a7fc20c9da32e01945c6bf9cabfb1a7d5
Gustavo <whoisgustavolima@gmail.com>
2025-02-27 20:26:00 -0300

    003

--- 003/Main.java
+++ 003/Main.java
@@ -8,16 +8,6 @@ public class Main {
         person.setAge(18);
         person.save();
 
-        if (person.isAdult()) {
-            if (person.save()) {
-                System.out.println("Person successfully registered");
-            } else {
-                System.out.println("Some error ocurred. Please try again");
-            }
-        } else {
-            System.err.println("Age must be major than 18");
-        }
-    
     }
 
 }
