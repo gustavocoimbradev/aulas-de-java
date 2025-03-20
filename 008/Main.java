fbb65e293c6cc794f113a46ba0fa5332e3beb85e
Gustavo <whoisgustavolima@gmail.com>
2025-03-20 19:50:07 -0300

    008

---
+++ 008/Main.java
@@ -0,0 +1,16 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.name = "Gustavo";
+        student.age = 27;
+        student.ra = 123;
+
+        System.out.println("Name: "+student.name);
+        System.out.println("Age: "+student.age);
+        System.out.println("RA: "+student.ra);
+
+    }
+
+}
---
+++ 008/Person.java
@@ -0,0 +1,6 @@
+class Person {
+
+    public String name;
+    public Number age;
+
+}
